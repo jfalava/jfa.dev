b41869d97b2142915afe050a9d937bd91f2e2960
Jorge Fernando Álava <git@jfa.dev>
2024-05-05 21:45:34 +0200

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-
 import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [solidJs()]
+  integrations: [solidJs(), tailwind(), mdx()]
 });
