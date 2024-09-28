954afe47e5aa6271e5df1c7492a02b6dfddb2d78
Jorge Fernando Álava <git@jfa.dev>
2024-09-28 22:19:29 +0200

    ok i like this

--- landing-page/src/components/reusables/fetchers/GetLastCommit.tsx
+++ landing-page/src/components/reusables/fetchers/GetLastCommit.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 interface LastCommitProps {
   b: string;
   token?: string;
@@ -47,11 +46,53 @@ const LastCommit: React.FC<LastCommitProps> = ({ b, token }) => {
   }, [b, token]);
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="flex items-center dark:invert">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-auto w-6 dark:invert"
+          viewBox="0 0 24 24"
+        >
+          <circle cx="4" cy="12" r="3" fill="currentColor">
+            <animate
+              id="svgSpinners3DotsFade0"
+              fill="freeze"
+              attributeName="opacity"
+              begin="0;svgSpinners3DotsFade1.end-0.25s"
+              dur="0.75s"
+              values="1;0.2"
+            />
+          </circle>
+          <circle cx="12" cy="12" r="3" fill="currentColor" opacity="0.4">
+            <animate
+              fill="freeze"
+              attributeName="opacity"
+              begin="svgSpinners3DotsFade0.begin+0.15s"
+              dur="0.75s"
+              values="1;0.2"
+            />
+          </circle>
+          <circle cx="20" cy="12" r="3" fill="currentColor" opacity="0.3">
+            <animate
+              id="svgSpinners3DotsFade1"
+              fill="freeze"
+              attributeName="opacity"
+              begin="svgSpinners3DotsFade0.begin+0.3s"
+              dur="0.75s"
+              values="1;0.2"
+            />
+          </circle>
+        </svg>
+      </div>
+    );
   }
 
   if (error) {
-    return <p>Error: {error}</p>;
+    return (
+      <div className="flex items-center">
+        <p className="mobile-only:text-sm bold">Error: {error}</p>
+      </div>
+    );
   }
 
   return (
@@ -61,6 +102,7 @@ const LastCommit: React.FC<LastCommitProps> = ({ b, token }) => {
           href={commitLink.match(/\((.*?)\)/)?.[1] || "#"}
           target="_blank"
           rel="noopener noreferrer"
+          className="bold"
         >
           {commitLink.match(/\[(.*?)\]/)?.[1] || "Commit"}
         </a>
