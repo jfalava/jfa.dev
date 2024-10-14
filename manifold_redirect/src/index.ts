37c0b3257291421e2c833dce46db56c69a85e58a
Jorge Fernando Álava <git@jfa.dev>
2024-10-14 19:04:27 +0200

    prettier

--- manifold_redirect/src/index.ts
+++ manifold_redirect/src/index.ts
@@ -1,31 +1,31 @@
 export default {
-	async fetch(request: Request): Promise<Response> {
-		const url = new URL(request.url);
-		const { hostname, pathname, search } = url;
+  async fetch(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+    const { hostname, pathname, search } = url;
 
-		let destinationHost: string = 'jfa.dev';
-		let destinationPath: string = '/';
-		const statusCode = 301;
+    let destinationHost: string = "jfa.dev";
+    let destinationPath: string = "/";
+    const statusCode = 301;
 
-		switch (hostname) {
-			case 'cv.jfa.dev':
-				destinationPath = '/cv';
-				break;
-			case 'landing.jfa.dev':
-				destinationPath = '';
-				break;
-			case 'links.jfa.dev':
-				destinationPath = '';
-				break;
-			case 'cv.jfalava.eu':
-				destinationPath = '/cv';
-				break;
-			case 'www.jfa.dev':
-				destinationPath = '/';
-				break;
-		}
+    switch (hostname) {
+      case "cv.jfa.dev":
+        destinationPath = "/cv";
+        break;
+      case "landing.jfa.dev":
+        destinationPath = "";
+        break;
+      case "links.jfa.dev":
+        destinationPath = "";
+        break;
+      case "cv.jfalava.eu":
+        destinationPath = "/cv";
+        break;
+      case "www.jfa.dev":
+        destinationPath = "/";
+        break;
+    }
 
-		const destinationURL = `https://${destinationHost}${destinationPath}${pathname}${search}`;
-		return Response.redirect(destinationURL, statusCode);
-	},
+    const destinationURL = `https://${destinationHost}${destinationPath}${pathname}${search}`;
+    return Response.redirect(destinationURL, statusCode);
+  },
 } satisfies ExportedHandler;
