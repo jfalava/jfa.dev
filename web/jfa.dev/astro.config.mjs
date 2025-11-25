93a7bf129c0ab839dbf637bf330eab6f5eadc1ed
Jorge Fernando Álava <git@jfa.dev>
2025-11-26 00:16:21 +0100

    chore: catalog package updates

--- web/jfa.dev/astro.config.mjs
+++ web/jfa.dev/astro.config.mjs
@@ -5,6 +5,8 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jfa.dev",
+  output: "server",
   vite: { plugins: [tailwindcss()] },
   adapter: cloudflare({
     platformProxy: {
