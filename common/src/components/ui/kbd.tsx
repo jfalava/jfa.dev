3c2d2db7049e155a02d93bcdad943d2bc2f6ac9c
Jorge Fernando Álava <git@jfa.dev>
2026-08-17 00:30:23 +0200

    Update kbd.tsx

--- common/src/components/ui/kbd.tsx
+++ common/src/components/ui/kbd.tsx
@@ -1,6 +1,6 @@
-import { Keyboard as KbdPrimitive } from "react-aria-components"
+import { cn } from "../../lib/utils";
 
-import { cn } from "../../lib/utils"
+import { Keyboard as KbdPrimitive } from "react-aria-components";
 
 function Kbd({ className, ...props }: React.ComponentProps<"kbd">) {
   return (
@@ -8,11 +8,11 @@ function Kbd({ className, ...props }: React.ComponentProps<"kbd">) {
       data-slot="kbd"
       className={cn(
         "pointer-events-none inline-flex h-5 w-fit min-w-5 items-center justify-center gap-1 rounded-sm bg-muted px-1 font-sans text-xs font-medium text-muted-foreground select-none in-data-[slot=tooltip-content]:bg-background/20 in-data-[slot=tooltip-content]:text-background dark:in-data-[slot=tooltip-content]:bg-background/10 [&_svg:not([class*='size-'])]:size-3",
-        className
+        className,
       )}
       {...props}
     />
-  )
+  );
 }
 
 function KbdGroup({ className, ...props }: React.ComponentProps<"div">) {
@@ -22,7 +22,7 @@ function KbdGroup({ className, ...props }: React.ComponentProps<"div">) {
       className={cn("inline-flex items-center gap-1", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Kbd, KbdGroup }
+export { Kbd, KbdGroup };
